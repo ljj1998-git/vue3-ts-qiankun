c83c4d61473d71467de1cce30675448747473987
ljj <1029680498@qq.com>
2022-07-04 14:16:18 +0800

    提交

--- vue3-base/.eslintrc.js
+++ vue3-base/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'eslintno-multiple-empty-lines': 'off',
     semi: [2, 'always'], // 尾部使用分号“;”
     'vue/multi-word-component-names': 0,
     indent: ['error', 2], // 缩进2格
@@ -23,11 +24,33 @@ module.exports = {
     quotes: [2, 'single'], // 使用单引号
     'vue/html-closing-bracket-newline': 'off', // 不强制换行
     'vue/singleline-html-element-content-newline': 'off', // 不强制换行
-    'vue/max-attributes-per-line': ['error', {
-      singleline: { max: 5 },
-      multiline: { max: 5 },
-    }], // vue template模板元素第一行最多5个属性
     'linebreak-style': [0, 'error', 'windows'], // 解决 'LF' but found 'CRLF'
     // 其它的规则可以去eslint查看，根据自己需要进行添加
+    // Vue: Recommended rules to be closed or modify
+    'vue/require-default-prop': 0,
+    'vue/max-attributes-per-line': 0,
+    // Vue: Add extra rules
+    'vue/custom-event-name-casing': [2, 'camelCase'],
+    'vue/no-v-text': 1,
+    'vue/padding-line-between-blocks': 1,
+    'vue/require-direct-export': 1,
+    // Allow @ts-ignore comment
+    '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-unused-vars': 1,
+    '@typescript-eslint/no-empty-function': 1,
+    '@typescript-eslint/no-explicit-any': 0,
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-param-reassign': 0,
+    'prefer-regex-literals': 0,
+    'import/no-extraneous-dependencies': 0,
   },
 };
